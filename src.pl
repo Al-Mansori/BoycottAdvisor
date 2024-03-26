9d1601cf192fb82cd210f02779e0119c6f9ed1ba
Mohammed Al-Mansori <95043517+Al-Mansori@users.noreply.github.com>
2024-03-26 19:35:02 +0200

    Update src.pl

--- src.pl
+++ src.pl
@@ -1,9 +1,10 @@
+:-consult('data.pl').
 :- dynamic item/3.
 :- dynamic alternative/2.
 :- dynamic boycott_company/2.
-:-consult('data.pl').
 
-% 1
+% 1. List all orders of a specific customer (as a list)
+
 list_orders(CustUserName, Orders) :-
 	customer(CustID, CustUserName),
 	list_orders_helper(CustID, [], Orders).
@@ -21,7 +22,9 @@ is_member(Item, [Item|_]).
 is_member(Item, [_|Tail]) :-
 	is_member(Item, Tail).
 
-% 2
+
+
+% 2. Get the number of orders of a specific customer given customer id.
 
 countOrdersOfCustomer(CustUserName, Count) :-
 	list_orders(CustUserName, Orders),
@@ -33,13 +36,16 @@ list_len([_|Tail], Length) :-
 	Length is TailLength + 1.
 
 
-% 3
-getItemsInOrderById(CustomerName,OrderId,Items):-
-    customer(CustomerId,CustomerName),
-    order(CustomerId,OrderId,Items),!.
+
+% 3. List all items in a specific customer order given customer id and order id.
+
+getItemsInOrderById(CustUserName, OrderID, Items) :-
+	customer(CustID, CustUserName),
+	order(CustID, OrderID, Items).
 
 
-% 4
+
+% 4. Get the num of items in a specific customer order given customer Name and order id.
 
 getNumOfItems(CustUserName, OrderID, ItemsCount) :-
 	customer(CustID, CustUserName),
@@ -48,7 +54,8 @@ getNumOfItems(CustUserName, OrderID, ItemsCount) :-
 
 
 
-% 5
+% 5. Calculate the price of a given order given Customer Name and order id
+
 calcPriceOfOrder(CustUserName, OrderID, TotalPrice) :-
 	customer(CustID, CustUserName),
 	order(CustID, OrderID, Items),
@@ -62,7 +69,7 @@ items_price([Head|Tail], TotalPrice) :-
 
 
 
-% 6
+% 6. Given the item name or company name, determine whether we need to boycott or not.
 
 isBoycott(ItemName) :-
 	item(ItemName, CompanyName, _),
@@ -72,7 +79,7 @@ isBoycott(CompanyName) :-
 
 
 
-% 7
+% 7. Given the company name or an item name, find the justification why you need to boycott this company/item.
 
 whyToBoycott(CompanyName, Justification) :-
 	boycott_company(CompanyName, Justification).
@@ -82,7 +89,8 @@ whyToBoycott(ItemName, Justification) :-
 
 
 
-% 8
+% 8. Given an username and order ID, remove all the boycott items from this order.
+
 removeBoycottItemsFromAnOrder(CustUserName, OrderID, NewItems) :-
 	customer(CustID, CustUserName),
 	order(CustID, OrderID, Items),
@@ -98,51 +106,45 @@ remove_boycott_items([Head|Tail], [Head|NewTail]) :-
 	remove_boycott_items(Tail, NewTail).
 
 
-% 9
-recursiveFunctionReplaceBoycottItemsFromAnOrder([H|T],Acc,ResultList):-
-    item(H,ComName,_),
-    boycott_company(ComName,_),
-    alternative(H,Alter),
-    recursiveFunctionReplaceBoycottItemsFromAnOrder(T, [Alter|Acc],ResultList).
- 
 
-recursiveFunctionReplaceBoycottItemsFromAnOrder([H|T],Acc,ResultList):-
-    item(H,ComName,_),
-    \+ boycott_company(ComName,_),
-    recursiveFunctionReplaceBoycottItemsFromAnOrder(T, [H|Acc],ResultList).
+% 9. Given an username and order ID, update the order such that all boycott items are replaced by an alternative (if exists).
 
+replaceBoycottItemsFromAnOrder(CustUserName, OrderID, NewItems) :-
+	customer(CustID, CustUserName),
+	order(CustID, OrderID, Items),
+	replace_boycott_items(Items, NewItems).
 
-
-recursiveFunctionReplaceBoycottItemsFromAnOrder(_,ResultList,ResultList).
-
-
-replaceBoycottItemsFromAnOrder(Username, OrderID, NewList):-
-    customer(CustID,Username),
-    order(CustID,OrderID,OldList),
-    recursiveFunctionReplaceBoycottItemsFromAnOrder(OldList,[],NewList),
-    !.
-
-
-% 10
-
-calcPriceAfterReplacingBoycottItemsFromAnOrder(Username, OrderID, NewList, TotalPrice):-
-    replaceBoycottItemsFromAnOrder(Username,OrderID,NewList),
-    calculateTotalPrice(NewList,0,TotalPrice).
-
-
-% 11
-getTheDifferenceInPriceBetweenItemAndAlternative(BoycottItem, A, DiffPrice):-
-    item(BoycottItem,_,Price1),
-    alternative(BoycottItem,A),
-    item(A,_,Price2),
-    DiffPrice is Price1 - Price2.
+replace_boycott_items([], []).
+replace_boycott_items([Head|Tail], [NewHead|NewTail]) :- 
+	item(Head, CompanyName, _),
+	boycott_company(CompanyName, _),
+	alternative(Head, NewHead),
+	!,
+	replace_boycott_items(Tail, NewTail).
+replace_boycott_items([Head|Tail], [Head|NewTail]) :-
+	replace_boycott_items(Tail, NewTail).
 
 
 
+% 10. Given an username and order ID, calculate the price of the order after replacing all boycott items by its alternative (if it exists).
+
+calcPriceAfterReplacingBoycottItemsFromAnOrder(CustUserName, OrderID, NewItems, NewPrice) :-
+	replaceBoycottItemsFromAnOrder(CustUserName, OrderID, NewItems),
+	items_price(NewItems, NewPrice).
 
 
 
-% 12
+% 11. calculate the difference in price between the boycott item and its alternative.
+
+getTheDifferenceInPriceBetweenItemAndAlternative(BoycottItem, AltItem, Diff) :-
+	alternative(BoycottItem, AltItem),
+	item(BoycottItem, _, BoycottPrice),
+	item(AltItem, _, AltPrice),
+	Diff is BoycottPrice - AltPrice.
+
+
+
+% 12. Insert/Remove (1)item, (2)alternative and (3)new boycott company to/from the knowledge base.
 
 add_item(ItemName, CompanyName, Price) :- assert(item(ItemName, CompanyName, Price)).
 remove_item(ItemName, CompanyName, Price) :- retract(item(ItemName, CompanyName, Price)).
@@ -152,3 +154,4 @@ remove_alternative(ItemName, AlternativeItem) :- retract(alternative(ItemName, A
 
 add_boycott_company(CompanyName, Justification) :- assert(boycott_company(CompanyName, Justification)).
 remove_boycott_company(CompanyName, Justification) :- retract(boycott_company(CompanyName, Justification)).
+
