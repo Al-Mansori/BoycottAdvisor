d8253441a12d398cb0f82bb9d0ef10ab5284adab
Mohammed Al-Mansori <95043517+Al-Mansori@users.noreply.github.com>
2024-03-26 19:29:08 +0200

    Update src.pl

--- src.pl
+++ src.pl
@@ -1,6 +1,9 @@
+:- dynamic item/3.
+:- dynamic alternative/2.
+:- dynamic boycott_company/2.
 :-consult('data.pl').
 
-%1
+% 1
 list_orders(CustUserName, Orders) :-
 	customer(CustID, CustUserName),
 	list_orders_helper(CustID, [], Orders).
@@ -18,7 +21,7 @@ is_member(Item, [Item|_]).
 is_member(Item, [_|Tail]) :-
 	is_member(Item, Tail).
 
-%2
+% 2
 
 countOrdersOfCustomer(CustUserName, Count) :-
 	list_orders(CustUserName, Orders),
@@ -30,59 +33,72 @@ list_len([_|Tail], Length) :-
 	Length is TailLength + 1.
 
 
-%3
+% 3
 getItemsInOrderById(CustomerName,OrderId,Items):-
     customer(CustomerId,CustomerName),
     order(CustomerId,OrderId,Items),!.
 
 
-%  4
-num_items_in_order(CustomerName, OrderID, ItemsCount) :-
-    customer(CustID, CustUserName),
-    order(CustID, OrderID, Items),
-    list_len(Items, ItemsCount).
+% 4
+
+getNumOfItems(CustUserName, OrderID, ItemsCount) :-
+	customer(CustID, CustUserName),
+	order(CustID, OrderID, Items),
+	list_len(Items, ItemsCount).
 
 
-%  5
-calcPriceOfOrder(CustomerName, OrderID, TotalPrice) :-
-    customer(CustomerID, CustomerName),
-    order(CustomerID, OrderID, Items),
-    calculateTotalPrice(Items, 0, TotalPrice).
 
-calculateTotalPrice([], Acc, Acc).
+% 5
+calcPriceOfOrder(CustUserName, OrderID, TotalPrice) :-
+	customer(CustID, CustUserName),
+	order(CustID, OrderID, Items),
+	items_price(Items, TotalPrice).
 
-calculateTotalPrice([Item|Rest], Acc, TotalPrice) :-
-    item(Item, _, Price),
-    NewAcc is Acc + Price,
-    calculateTotalPrice(Rest, NewAcc, TotalPrice).
+items_price([], 0).
+items_price([Head|Tail], TotalPrice) :-
+	item(Head, _, HeadPrice),
+	items_price(Tail, TailPrice),
+	TotalPrice is HeadPrice + TailPrice.
+
 
 
 % 6
-isBoycott(ItemOrCompanyName) :-
-    boycott_company(ItemOrCompanyName, _).
-
 
 isBoycott(ItemName) :-
-    item(ItemName, Company, _),
-    boycott_company(Company, _).
+	item(ItemName, CompanyName, _),
+	boycott_company(CompanyName, _).
+isBoycott(CompanyName) :-
+	boycott_company(CompanyName, _).
+
+
 
 % 7
-whyToBoycott(ItemOrCompanyName, Justification) :-
-    item(ItemOrCompanyName, Company, _),
-    boycott_company(Company, Justification).
 
-whyToBoycott(ItemOrCompanyName, Justification) :-
-    % Check if the company itself is boycotted and get the justification
-    boycott_company(ItemOrCompanyName, Justification).
-
-
-%8
-
-%...
+whyToBoycott(CompanyName, Justification) :-
+	boycott_company(CompanyName, Justification).
+whyToBoycott(ItemName, Justification) :-
+	item(ItemName, CompanyName, _),
+	boycott_company(CompanyName, Justification).
 
 
 
-%9
+% 8
+removeBoycottItemsFromAnOrder(CustUserName, OrderID, NewItems) :-
+	customer(CustID, CustUserName),
+	order(CustID, OrderID, Items),
+	remove_boycott_items(Items, NewItems).
+
+remove_boycott_items([], []).
+remove_boycott_items([Head|Tail], NewTail) :- 
+	item(Head, CompanyName, _),
+	boycott_company(CompanyName, _),
+	!,
+	remove_boycott_items(Tail, NewTail).
+remove_boycott_items([Head|Tail], [Head|NewTail]) :-
+	remove_boycott_items(Tail, NewTail).
+
+
+% 9
 recursiveFunctionReplaceBoycottItemsFromAnOrder([H|T],Acc,ResultList):-
     item(H,ComName,_),
     boycott_company(ComName,_),
@@ -107,14 +123,14 @@ replaceBoycottItemsFromAnOrder(Username, OrderID, NewList):-
     !.
 
 
-%10
+% 10
 
 calcPriceAfterReplacingBoycottItemsFromAnOrder(Username, OrderID, NewList, TotalPrice):-
     replaceBoycottItemsFromAnOrder(Username,OrderID,NewList),
     calculateTotalPrice(NewList,0,TotalPrice).
 
 
-%11
+% 11
 getTheDifferenceInPriceBetweenItemAndAlternative(BoycottItem, A, DiffPrice):-
     item(BoycottItem,_,Price1),
     alternative(BoycottItem,A),
@@ -123,7 +139,16 @@ getTheDifferenceInPriceBetweenItemAndAlternative(BoycottItem, A, DiffPrice):-
 
 
 
-%12
 
 
-%...
+
+% 12
+
+add_item(ItemName, CompanyName, Price) :- assert(item(ItemName, CompanyName, Price)).
+remove_item(ItemName, CompanyName, Price) :- retract(item(ItemName, CompanyName, Price)).
+
+add_alternative(ItemName, AlternativeItem) :- assert(alternative(ItemName, AlternativeItem)).
+remove_alternative(ItemName, AlternativeItem) :- retract(alternative(ItemName, AlternativeItem)).
+
+add_boycott_company(CompanyName, Justification) :- assert(boycott_company(CompanyName, Justification)).
+remove_boycott_company(CompanyName, Justification) :- retract(boycott_company(CompanyName, Justification)).
